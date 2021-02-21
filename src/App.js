825789cc3aad8de8d2997b67befd6478d7b0cf09
PovilasU <povilas.urbonas85@gmail.com>
2021-02-21 22:05:15 +0100

    add css

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>webpack-lighthouse-100</h1>
+      <h1>Webpack-lighthouse-100</h1>
       <p>React App</p>
+      <p>Website is under construction.</p>
+      <p>Resources:</p>
+      <li>https://www.mankocode.com/reactjs-setup-with-webpack-and-babel/</li>
+      <li>https://www.valentinog.com/blog/webpack/</li>
+      <li>https://github.com/PovilasU/webpack-lighthouse-100</li>
     </div>
   );
 }
