8cca13249e886891bac67113bf99df323f203d08
PovilasU <povilas.urbonas85@gmail.com>
2021-02-23 00:51:31 +0100

    Dynamic load

--- src/index.js
+++ src/index.js
@@ -5,4 +5,15 @@ import ReactDOM from 'react-dom';
 import './style.scss';
 import App from './App';
 
+const getUserModule = () =>
+  import(/* webpackChunkName: "usersAPI" */ './common/usersAPI');
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  getUserModule().then(({ getUsers }) => {
+    getUsers().then((json) => console.log(json));
+  });
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
