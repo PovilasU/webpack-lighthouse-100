336f8a19a133e3477fea18d57b43ccf0c50e2187
PovilasU <povilas.urbonas85@gmail.com>
2021-02-23 01:21:52 +0100

    dynamically loading component

--- src/index.js
+++ src/index.js
@@ -5,4 +5,17 @@ import ReactDOM from 'react-dom';
 import './style.scss';
 import App from './App';
 
+async function getComponent() {
+  const element = document.createElement('div');
+  const { default: _ } = await import('lodash');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
